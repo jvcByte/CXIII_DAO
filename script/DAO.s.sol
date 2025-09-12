f1bce0569837d7b1aed93ff8f6bc0aef39bad44d
jvcbyte <jvc8463@gmail.com>
2025-09-12 19:38:46 +0100

    Move to writing scripts

--- script/DAO.s.sol
+++ script/DAO.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import {Script, console} from "forge-std/Script.sol";
 import {Cohort_CXIII_DAO} from "../src/Dao.sol";
 
-contract Cohort_CXIII_DAOScript is Script {
+contract Cohort_CXIII_DAO_Script is Script {
     Cohort_CXIII_DAO public dao;
 
     function setUp() public {}
