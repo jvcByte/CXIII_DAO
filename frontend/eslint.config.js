952b89a9ea74da3ccd3026bc6abe3d2d5c3581b2
jvcbyte <jvc8463@gmail.com>
2025-09-13 11:20:38 +0100

    eslint ignore routeTree.gen.ts

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,10 +3,16 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+export default defineConfig([
+  globalIgnores(
+    [
+      'dist',
+      '!node_modules/',
+      'src/routeTree.gen.ts',
+    ]
+  ),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
