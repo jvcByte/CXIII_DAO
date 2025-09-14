2c61048c9d5eb9f822b06879cd38818ea64b28d2
jvcbyte <jvc8463@gmail.com>
2025-09-14 15:51:07 +0100

    update root layout to extend contexts: deps injection

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,17 +1,31 @@
 import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { Toaster } from 'react-hot-toast'
 
 import { QueryClient } from '@tanstack/react-query'
+import { NotFoundError } from '@/components/errors/not-found-error'
+import { GeneralError } from '@/components/errors/general-error'
 
 interface MyRouterContext {
     queryClient: QueryClient
-} 
+}
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
     component: () => (
         <>
             <Outlet />
-            <TanStackRouterDevtools position="bottom-right" initialIsOpen={false} />
+            <Toaster
+                toastOptions={{
+                    duration: 5000,
+                }}
+                position="top-center"
+            />
+            <TanStackRouterDevtools
+                position="bottom-right"
+                initialIsOpen={false}
+            />
         </>
     ),
+    notFoundComponent: NotFoundError,
+    errorComponent: GeneralError,
 })
