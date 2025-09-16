18533007979ff0812fa6c73e614762e487dfa33b
jvcbyte <jvc8463@gmail.com>
2025-09-16 16:56:04 +0100

    Implement auth

--- frontend/src/components/ui/custom-ui/customeButton.tsx
+++ frontend/src/components/ui/custom-ui/customeButton.tsx
@@ -12,10 +12,10 @@ export const CustomConnectKitButton = () => {
             size="lg"
             className="border-2 border-stone-300 dark:border-stone-400 text-base"
           >
-            {isConnecting
-              ? "Signing In..."
-              : isConnected
-                ? (ensName ?? truncatedAddress)
+            {isConnected
+              ? (ensName ?? truncatedAddress)
+              : isConnecting
+                ? "Signing In..."
                 : "Sign In"}
           </Button>
         );
