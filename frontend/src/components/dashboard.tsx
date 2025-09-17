dfe4bba7fe3b332ff9e4d007b73fb79bd88b2348
jvcbyte <jvc8463@gmail.com>
2025-09-17 15:33:04 +0100

    implemented  events watching for proposal creation

--- frontend/src/components/dashboard.tsx
+++ frontend/src/components/dashboard.tsx
@@ -1,8 +1,43 @@
-import { useReadContract } from "wagmi";
+import { useReadContract, usePublicClient } from "wagmi";
+import { useQuery } from "@tanstack/react-query";
 import contracts from "@/contracts/types";
+import { DAO_EVENTS } from "@/contracts/events";
 // import { formatNumber } from "@/lib/utils";
 
+export interface proposalCountEventProps {
+  id: bigint;
+  description: string;
+  deadline: bigint;
+}
+
 export function Dashboard() {
+  const publicClient = usePublicClient();
+
+  const { data: proposalEvents } = useQuery({
+    queryKey: ["proposal-events", contracts.CXII_DAO.address],
+    queryFn: async () => {
+      if (!publicClient) return [];
+
+      const logs = await publicClient.getLogs({
+        address: contracts.CXII_DAO.address,
+        event: DAO_EVENTS.ProposalCreated,
+        fromBlock: "earliest",
+        toBlock: "latest",
+      });
+
+      return logs.map((log) => ({
+        id: log.args.id,
+        description: log.args.description,
+        deadline: log.args.deadline,
+        blockNumber: log.blockNumber,
+        transactionHash: log.transactionHash,
+        logIndex: log.logIndex,
+      }));
+    },
+    enabled: !!publicClient,
+    staleTime: 1000,
+  });
+  console.log("Prposal Event: ", proposalEvents);
   const { data: proposalCounts } = useReadContract({
     ...contracts.CXII_DAO,
     functionName: "proposalCount",
@@ -11,7 +46,7 @@ export function Dashboard() {
   console.log("Prposal Count: ", proposalCounts);
   return (
     <div className="flex flex-col gap-6 p-4 max-w-[1500px] mx-auto">
-      <div className="flex items-center justify-between">
+      <div className="flex proposalCountsitems-center justify-between">
         <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
       </div>
       <div className="grid gap-4 grid-cols-2 md:grid-cols-2 lg:grid-cols-4">
@@ -19,7 +54,7 @@ export function Dashboard() {
           <h3 className="text-sm font-medium text-muted-foreground">
             Total Proposals
           </h3>
-          <div className="text-2xl font-bold">{proposalCounts}</div>
+          <div className="text-2xl font-bold">{proposalCounts || "0"}</div>
         </div>
         <div className="rounded-lg border bg-card p-6">
           <h3 className="text-sm font-medium text-muted-foreground">
